30d99a4953832f7e8db1b2d0261b5ae9cda780d9
Akshit Sharma <akshit.sharma@Akshits-MacBook-Pro.local>
2023-04-06 14:39:58 +0200

    ertdcomm

---
+++ newghghtf.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+
+    access_key = "${secrets.aws_access_key}"
+
+    secret_key = "${secrets.aws_secret_key}"
+
+    region = "${secrets.region}"
+
+}
+
+# module "s3" {
+
+#     source = "<path-to-S3-folder>"
+
+#     bucket_name = "your_bucket_name"       
+
+# }
+
+resource "aws_s3_bucket" "2332hwte46347dhdjkfe" {
+
+    bucket = "${secrets.bucket_name}" 
+
+    acl = "${secrets.acl_value}"   
+
+}
